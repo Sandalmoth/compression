d762d780a4adb859b82cfcd702716538f9961fa5
Jonathan Lindström <jonathan.lindstrom@lnu.se>
2017-06-15 11:10:15 +0200

    ibwt nonfunctional;

--- main.cpp
+++ main.cpp
@@ -9,13 +9,21 @@ using namespace std;
 
 int main() {
   vector<char> s {'b', 'i', 'c', 'y', 'c', 'l', 'e'};
-  char s2[] = "The quick brown fox jumps over the lazy dog. One fish, two fish, red fish, nuke fish.";
+  // char s2[] = "The quick brown fox jumps over the lazy dog. One fish, two fish, red fish, nuke fish.";
+  char s2[] = "onethree";
   for (auto c: s) cout << c;
   cout << endl;
-  cout << transforms::bwt(s.begin(), s.end()) - s.begin() << endl;
+  int z = transforms::bwt(s.begin(), s.end()) - s.begin();
+  cout << z << endl;
+  for (auto c: s) cout << c;
+  cout << endl;
+  transforms::ibwt(s.begin(), s.end(), s.begin() + z);
   for (auto c: s) cout << c;
   cout << endl;
   cout << s2 << endl;
-  cout << transforms::bwt(s2, s2+sizeof(s2)-1) - s2 << endl;
+  int z2 = transforms::bwt(s2, s2+sizeof(s2)-1) - s2;
+  cout << z2 << endl;
+  cout << s2 << endl;
+  transforms::ibwt(s2, s2+sizeof(s2)-1, s2+z2);
   cout << s2 << endl;
 }
--- transforms.h
+++ transforms.h
@@ -5,6 +5,7 @@
 #include <map>
 #include <tuple>
 #include <algorithm>
+#include <list>
 
 #include <iostream>
 
@@ -59,8 +60,59 @@ namespace transforms {
   template <typename Titer>
   void ibwt(Titer first, Titer last, Titer end) {
     typedef typename std::remove_reference<decltype(*std::declval<Titer>())>::type value_type;
-    typedef std::tuple<value_type, Titer, bool> movement;
+    typedef std::pair<bool, std::list<value_type>> full_rotation;
+    std::vector<full_rotation> matrix;
 
+    // Build last column of matrix with end char marker
+    for (auto it = first; it != last; ++it) {
+      matrix.push_back(std::make_pair(false, std::list<value_type>{*it}));
+      if (it == end) {
+        matrix.back().first = true;
+      }
+    }
+
+    // complete matrix
+    auto it_c = first;
+    ++it_c;
+    for (; it_c != last; ++it_c) {
+      for (auto m: matrix) {
+        for (auto x: m.second) {
+          std::cout << x << ' ';
+        }
+        if (m.first) std::cout << '*';
+        std::cout << std::endl;
+      }
+      std::sort(matrix.begin(), matrix.end()
+                , [](auto &a, auto &b) {
+                  auto ait = a.second.begin();
+                  auto bit = b.second.begin();
+                  for (; ait != a.second.end(); ++ait, ++bit) {
+                    if (*ait < *bit) {
+                      return true;
+                    }
+                    if ((*ait == *bit) && (*ait = --a.second.end())) {
+                      if (a.first)
+                        return true;
+                    }
+                  }
+                  return false;
+                });
+      auto mit = matrix.begin();
+      auto rit = first;
+      for (; rit != last; ++rit, ++mit) {
+        mit->second.push_front(*rit);
+      }
+    }
+
+    // copy row with ending marker
+    for (auto m: matrix) {
+      if (m.first) {
+        for(auto x: m.second) {
+          *first = x;
+          ++first;
+        }
+      }
+    }
   }
 
 
