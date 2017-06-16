0bfc24153e6d39fb64a04e61682e0a661b41ff94
Jonathan <j@localhost.localdomain>
2017-06-16 23:33:26 +0200

    Ballmers peak work on the huffman encoder. might work. maybe.

---
+++ huff.h
@@ -0,0 +1,168 @@
+#ifndef __HUFF_H__
+#define __HUFF_H__
+
+
+#include <vector>
+#include <map>
+#include <queue>
+#include <cstdlib>
+
+#include <iostream>
+
+std::string toBinary(int n)
+{
+  std::string r;
+  while(n!=0) {r=(n%2==0 ?"0":"1")+r; n/=2;}
+  return r;
+}
+
+
+namespace huff {
+
+
+  // Consider less c-like implementation
+  // but well, at least it uses nullptr :)
+  struct Node {
+    Node *parent = nullptr;
+    Node *child0 = nullptr;
+    Node *child1 = nullptr;
+    double weight;
+    unsigned char symbol;
+  };
+
+  class nodecomp {
+  public:
+    bool operator()(Node *a, Node *b) {
+      return a->weight > b->weight;
+      // fuck me i though priority queue picked the lowest element first.
+      // so dumb.
+    }
+  };
+
+
+  std::map<unsigned char, std::vector<bool>> encode(unsigned char *v, int c, unsigned char *&out, int &outsize) {
+    // count occurrences of bytes.
+    std::map<unsigned char, int> counts;
+    for (int i = 0; i < c; ++i) {
+      auto it = counts.find(v[i]);
+      if (it != counts.end()) {
+        ++it->second;
+      } else {
+        counts[v[i]] = 1;
+      }
+    }
+    for (auto p: counts) {
+      std::cout << p.first << '\t' << p.second << std::endl;
+    }
+
+    // Create leaf nodes and put them into a priority queue
+    std::priority_queue<Node *, std::vector<Node *>, nodecomp> leaves;
+    std::vector<Node> nodes; // circumvent construction/destruction messing with my pointers yo
+    nodes.reserve(2*c); // also, when we add new parents later, we need extra space (thus the 2)
+    // otherwise, reallocations would fuck shit up.
+    for (auto x: counts) {
+      Node n;
+      n.weight = static_cast<double>(x.second) / static_cast<double>(c);
+      n.symbol = x.first;
+      nodes.push_back(n);
+      leaves.push(&nodes.back());
+    }
+
+    // Now assemble a binary tree
+    while (leaves.size() > 1) {
+      auto n1 = leaves.top();
+      leaves.pop();
+      auto n2 = leaves.top();
+      leaves.pop();
+      Node p;
+      p.weight = n1->weight + n2->weight;
+      // order here is probably important
+      p.child1 = n1;
+      p.child0 = n2;
+      nodes.push_back(p);
+      leaves.push(&nodes.back());
+      n1->parent = &nodes.back();
+      n2->parent = &nodes.back();
+    }
+
+    for (auto &l: nodes) {
+      std::cout << &l - &(*nodes.begin()) << ' ' << l.parent - &(*nodes.begin()) << ' ' << l.weight << std::endl;
+    }
+
+    // presumably, at this point there is one item in leaves, and it's the top node.
+    std::cout << "There are some items in leaves: " << leaves.size() << std::endl;
+
+    std::map<unsigned char, std::vector<bool>> huffs; // huffs: an excellent word.
+    // the vector<bool> representation seems stupid, but eh
+    // at least it's space efficient internally lol
+    for (auto &n: nodes) {
+      // this if identifies leaves that have a symbol
+      // though maybe looking for the symbol makes more sense?
+      // not that it really matters.
+      if (n.child0 == nullptr) {
+        auto pn = &n; // previous node
+        auto cn = n.parent; // node we just moved into; so current node
+        std::vector<bool> huff;
+        while (true) {
+          cn = pn->parent;
+          bool y = (cn->child1 == pn); // is this the node in child 1? then true (i.e. 1) else 0;
+          huff.push_back(y); // i could std::move here i suppose. I wonder if that's faster?
+          // probably emplace with the expression. I'm sure the compiler works it out anyway though :P
+          pn = cn;
+          if (cn->parent == nullptr)
+            break;
+        }
+        huffs[n.symbol] = huff;
+      }
+    }
+    // print 'em
+    for (auto &h: huffs) {
+      std::cout << h.first << '\t';
+      for (auto x: h.second) {
+        std::cout << x << ' ';
+      }
+      std::cout << std::endl;
+    }
+
+    // calculate number of bytes needed to store code
+    int n_bits = 0;
+    for (auto &x: counts) {
+      n_bits += x.second * huffs[x.first].size();
+    }
+    std::cout << n_bits << std::endl;
+    int n_bytes = n_bits/8 + 1;
+    std::cout << n_bytes << std::endl;
+
+    // allocate bytes, and store it.
+    outsize = n_bytes;
+    int bitpos = 0; // position in byte
+    unsigned char w = 0; // workbyte
+    int outpos = 0;
+    out = reinterpret_cast<unsigned char*>(malloc(outsize)); // who cares about checking for NULL
+    for (int i = 0; i < c; ++i) {
+      // grab the huffman code;
+      auto h = huffs[v[i]];
+      for (auto b: h) {
+        w = w << 1; // god i hope i get the endianness right
+        // also, if your system has the wrong endianness, too bad >:D
+        // is that even a thing that can happen?
+        w = w | b;
+        ++bitpos;
+        std::cout << toBinary(w) << std::endl;
+        if (bitpos == 8) {
+          out[outpos] = w;
+          ++outpos;
+          w = 0;
+        }
+      }
+    }
+    // who the fuck knows if this works or not. we'll see tomorrow when I make the decoder.
+    // my primary concern is that the huffman code making is wrong.
+    return huffs;
+  }
+
+
+} // end namespace huff
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 
+#include "huff.h"
 #include "transforms.h"
 
 
@@ -9,7 +10,8 @@ using namespace std;
 
 int main() {
   vector<char> s {'b', 'i', 'c', 'y', 'c', 'l', 'e'};
-  char s2[] = "The quick brown fox jumps over the lazy dog. One fish, two fish, red fish, nuke fish.";
+  // char s2[] = "The quick brown fox jumps over the lazy dog. One fish, two fish, red fish, nuke fish.";
+  char s2[] = "a dead dad ceded a bad babe a beaded abaca bed";
   int lens2 = sizeof(s2);
   double s3[] = {3.14, 33.8, 1138.007};
   int lens3 = sizeof(s3);
@@ -22,6 +24,9 @@ int main() {
   transforms::nd_mtf(reinterpret_cast<unsigned char *>(s.data()), s.size());
   for (auto c: s) cout << c;
   cout << ' ' << endl;
+  unsigned char *comp;
+  int os;
+  huff::encode(reinterpret_cast<unsigned char *>(s.data()), s.size(), comp, os);
   transforms::nd_imtf(reinterpret_cast<unsigned char *>(s.data()), s.size());
   for (auto c: s) cout << c;
   cout << ' ' << endl;
@@ -34,6 +39,9 @@ int main() {
   cout << s2 << ' ' << z2 << endl;
   transforms::nd_mtf(reinterpret_cast<unsigned char *>(s2), lens2 - 1);
   cout << s2 << endl;
+  unsigned char *comp2;
+  int os2;
+  huff::encode(reinterpret_cast<unsigned char *>(s2), lens2 - 1, comp2, os2);
   transforms::nd_imtf(reinterpret_cast<unsigned char *>(s2), lens2 - 1);
   cout << s2 << endl;
   transforms::ibwt(s2, s2+lens2-1, s2+z2);
@@ -47,6 +55,9 @@ int main() {
   transforms::nd_mtf(reinterpret_cast<unsigned char *>(s3), lens3);
   for (auto c: s3) cout << c << ' ';
   cout << ' ' << endl;
+  unsigned char *comp3;
+  int os3;
+  huff::encode(reinterpret_cast<unsigned char *>(s3), lens3, comp3, os3);
   transforms::nd_imtf(reinterpret_cast<unsigned char *>(s3), lens3);
   for (auto c: s3) cout << c << ' ';
   cout << ' ' << endl;
